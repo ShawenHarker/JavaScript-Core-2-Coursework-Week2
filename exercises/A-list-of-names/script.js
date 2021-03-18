46297081ad2550b3d262c64096a7a0820b01d88c
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-03-18 16:02:26 +0200

    Exercises 1
    Completed the 1st exercise in the exercises folder.

--- exercises/A-list-of-names/script.js
+++ exercises/A-list-of-names/script.js
@@ -1,11 +1,20 @@
-function listOfNames(arrayOfPeople) {
-  let content = document.querySelector("#content");
-}
-
 let people = [
   { name: "Chris", job: "Teacher" },
   { name: "Joanna", job: "Student" },
   { name: "Boris", job: "Prime Minister" },
 ];
+
+function listOfNames(arrayOfPeople) {
+  let content = document.querySelector("#content");
+  
+  for (let i = 0; i < arrayOfPeople.length; i++) {
+    let createH1 = document.createElement("h1");
+    let createH2 = document.createElement("h2");
+    content.appendChild(createH1);
+    content.appendChild(createH2);
+    createH1.innerText = `${arrayOfPeople[i].name}`;
+    createH2.innerText = `${arrayOfPeople[i].job}`;
+  }
+}
 
 listOfNames(people);
