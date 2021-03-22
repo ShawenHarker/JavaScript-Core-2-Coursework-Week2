37abfb726a9931da17b482aa211ef696f8c52e92
ShawenHarker <ShawenHarker@users.noreply.github.com>
2021-03-22 22:49:22 +0200

    Exercise 5 - code fixed, working

--- exercises/E-list-of-colours/script.js
+++ exercises/E-list-of-colours/script.js
@@ -2,47 +2,26 @@ function listOfColours(coloursArr) {
   // Write your code here...
   let content = document.getElementById('content');
   let createPTag = document.createElement('p');
-  createPTag.innerText = "You have selected: ";
   content.appendChild(createPTag);
   let select = document.createElement('select');
-  createPTag.appendChild(select);
+  select.setAttribute('id', 'selectID');
+  content.appendChild(select);
   
-   let chooseColourOfArray = coloursArr.filter ((colour) => {
+  coloursArr.map((col) => {
     let option = document.createElement('option');
-    option.textContent = colour;
-    option.value = colour;
-    select.appendChild(option);
+    option.textContent = col;
+    return select.appendChild(option);
+  })
 
-    function chooseColour () {
-      if (option.value === 'red') {
-        createPTag.style.color = 'red';
-      }
-      else if (option.value === 'blue') {
-        createPTag.style.color = 'blue';
-      }
-      else if (option.value === 'green') {
-        createPTag.style.color = 'green';
-      }
-      else if (option.value === 'yellow') {
-        createPTag.style.color = 'yellow';
-      }
-      else if (option.value === 'pink') {
-        createPTag.style.color = 'pink';
-      }
-      else if (option.value === 'brown') {
-        createPTag.style.color = 'brown';
-      }
-    }
-    select.addEventListener('click', chooseColour);
-   })
-   .map ((colour) => colour[0]);
+  let getID = document.getElementById('selectID');
 
-   
- 
-  console.log(content);
+  getID.addEventListener('change', function () {
+    let findValue = getID.value;
+    createPTag.innerText = `You have selected: ${findValue}`;
+    createPTag.style.color = findValue;
+  }) 
 }
 
-
 const colours = ["red", "blue", "green", "yellow", "pink", "brown"];
-
+colours.push("lightgreen");
 listOfColours(colours);
