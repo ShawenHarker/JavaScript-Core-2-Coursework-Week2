bca2e19e35368149b22185b286fefbbf9f57f828
Shawen Aldred Harker <harkershawen@gmail.com>
2021-04-18 20:28:13 +0200

    Exercise F
    Event listener isn't being picked up, need to have a get it working all other code is working 100%.

--- exercises/F-highlight-words/script.js
+++ exercises/F-highlight-words/script.js
@@ -1,31 +1,45 @@
 function highlightWords(paragraph, colours) {
   // Write your code here...
   let content = document.getElementById('content');
-  let selectMenu = document.createElement('select');
-  let pTag1 = document.createElement("p");
-  content.appendChild(pTag1);
-  pTag1.innerHTML = `Please select a colour `;
-  pTag1.setAttribute('class', 'paragraph')
-  selectMenu.setAttribute("id", "selectID");
-  pTag1.appendChild(selectMenu);
 
-  colours.map((col) => {
-  let option = document.createElement("option");
-  selectMenu.appendChild(option);
-  option.innerText = col;
-  })
+  // paragraphArray takes the paragraph and turns it into an array with the split method and returns each word with an index inside of the array.
+  let paragraphArray = paragraph.split(' ');
 
-  let pTag = document.createElement('p');
+  let selectButton = document.createElement('select');
+  selectButton.setAttribute('id', 'selectID');
+  content.appendChild(selectButton);
+
+  // itirating through the list of colours and returning it in the select bar as an option to choose a colour from.
+  colours.forEach(colour => {
+    let option = document.createElement('option');
+    selectButton.appendChild(option);
+    option.innerText = colour;
+  });
+
+  let pTag = document.createElement("p");
   content.appendChild(pTag);
-  pTag.innerHTML = paragraph.split(paragraph.indexOf(''));
 
-
-  console.log(content);
+  // This step returns the array with each word wraped with a span wrapped around it and styled in CSS to look like a paragraph.
+  paragraphArray.forEach (word => {
+    let span = document.createElement('span');
+    span.setAttribute('class', 'span-tag')
+    pTag.appendChild(span);
+    span.innerText = word;
+  })
 }
+
+// Adding an event listener to my spans tag in the paragraph.
+// let getSpanClass = document.getElementsByClassName('span-tag');
+// let getSelectID = document.getElementById('selectID');
+
+// getSpanClass.addEventListener('click', () => {
+//   let getValue = getSelectID.value;
+//   span.style.backgroundColor = getValue;
+// });
 
 const paragraph =
   "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur convallis massa ut sem finibus ultrices. Phasellus hendrerit placerat libero non facilisis. Etiam tincidunt ut elit id elementum. Etiam accumsan semper ipsum, ac porttitor ex tempus non. Donec vitae massa condimentum, faucibus magna non, sagittis libero. Phasellus ullamcorper malesuada tellus at egestas. Duis volutpat turpis velit, ut bibendum tellus ornare ac. Nullam feugiat nisi fringilla eleifend scelerisque. Phasellus elit nulla, sodales eget consectetur sed, tincidunt vitae velit. Fusce eget tempus magna, vitae ultrices purus. Proin ac purus tellus. Curabitur rhoncus est quis libero egestas ultrices. Proin viverra a ex sed convallis. Fusce ut orci consectetur, placerat ipsum non, commodo neque. Nulla at dui velit. In ut accumsan libero. Duis ac lacus consectetur, posuere eros nec, aliquet nisl. Quisque sodales fringilla lacinia. Donec hendrerit ornare lectus, nec iaculis justo tincidunt et. Cras a varius libero, ac bibendum est. Aliquam molestie, leo non aliquam lobortis, nulla tortor hendrerit turpis, et ullamcorper magna diam quis elit. Maecenas elit libero, tincidunt sit amet lacinia sit amet, venenatis nec est.";
 
-const colours = ["yellow", "green", "blue", "none"];
+const colours = ["none", "yellow", "green", "blue"];
 
 highlightWords(paragraph, colours);
