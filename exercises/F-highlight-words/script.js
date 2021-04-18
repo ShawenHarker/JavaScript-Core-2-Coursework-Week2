9fbd8d389f700a7811f70b288cb806a9c60cf7a5
Shawen Aldred Harker <harkershawen@gmail.com>
2021-04-18 21:27:01 +0200

    Exercise F
    Changed ForEach to map, and I tried filter, none of them returns a value.

--- exercises/F-highlight-words/script.js
+++ exercises/F-highlight-words/script.js
@@ -10,29 +10,31 @@ function highlightWords(paragraph, colours) {
   content.appendChild(selectButton);
 
   // itirating through the list of colours and returning it in the select bar as an option to choose a colour from.
-  colours.forEach(colour => {
+  colours.map (colour => {
     let option = document.createElement('option');
-    selectButton.appendChild(option);
-    option.innerText = colour;
+    option.textContent = colour;
+    return selectButton.appendChild(option);
   });
 
   let pTag = document.createElement("p");
   content.appendChild(pTag);
 
   // This step returns the array with each word wraped with a span wrapped around it and styled in CSS to look like a paragraph.
-  paragraphArray.forEach (word => {
+  paragraphArray.map (word => {
     let span = document.createElement('span');
-    span.setAttribute('class', 'span-tag')
-    pTag.appendChild(span);
-    span.innerText = word;
+    span.setAttribute('class', 'span-tag');
+    span.textContent = word;
+    return pTag.appendChild(span);
   })
+  console.log(content);
 }
 
 // Adding an event listener to my spans tag in the paragraph.
-// let getSpanClass = document.getElementsByClassName('span-tag');
-// let getSelectID = document.getElementById('selectID');
+let getSelectID = document.getElementById("selectID");
+let getSpanClass = document.getElementsByClassName('span-tag');
 
-// getSpanClass.addEventListener('click', () => {
+console.log(getSelectID);
+// getSpanClass.addEventListener("click", () => {
 //   let getValue = getSelectID.value;
 //   span.style.backgroundColor = getValue;
 // });
