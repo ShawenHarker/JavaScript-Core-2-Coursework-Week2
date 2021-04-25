093f11bfcea71bed97d9fda2abc7c52553764b52
ShawenHarker <harkershawen@gmail.com>
2021-04-25 17:33:01 +0200

    Project 1 completed

--- projects/1-bike-project/js/main.js
+++ projects/1-bike-project/js/main.js
@@ -11,19 +11,19 @@ let blueButton = document.getElementById("blueBtn");
 let orangeButton = document.getElementById("orangeBtn");
 let greenButton = document.getElementById("greenBtn");
 
-// Adding an event listener to each button as spicified in the README.md file, giving each button its own unique style. 
-blueButton.addEventListener('click', () => {
-    jumbotron.style.backgroundColor = `#588fbd`;
-    donateButton.style.backgroundColor = `#ffa500`;
-    volunteerButton.style.backgroundColor = 'black';
-    volunteerButton.style.color = 'white';
+// Adding an event listener to each button as spicified in the README.md file, giving each button its own unique style.
+blueButton.addEventListener("click", () => {
+  jumbotron.style.backgroundColor = `#588fbd`;
+  donateButton.style.backgroundColor = `#ffa500`;
+  volunteerButton.style.backgroundColor = "black";
+  volunteerButton.style.color = "white";
 });
 
-orangeButton.addEventListener('click', () => {
+orangeButton.addEventListener("click", () => {
   jumbotron.style.backgroundColor = `#f0ad4e`;
   donateButton.style.backgroundColor = `#5751fd`;
   volunteerButton.style.backgroundColor = `#31b0d5`;
-  volunteerButton.style.color = 'white';
+  volunteerButton.style.color = "white";
 });
 
 greenButton.addEventListener("click", () => {
@@ -41,36 +41,67 @@ Getting targetted elements via their ids I inputted myself in the HTML.
 */
 
 // This function takes the validate functions, checks that it does validate and returns an alert message or a red background warning.
-function submitForm (event) {
-
+function submitForm(event) {
   event.preventDefault();
   event.stopPropagation();
 
   let returnForm = validate();
 
-  if (returnForm) {
-    document.getElementById('form-user').alert('Thank you for filing in the form.');
-  }
-  else {
-    document.getElementsByClassName('form-control').style.backgroundColor = 'red';
+  Object.keys(returnForm.fields).forEach((key) => {
+    if (returnForm.fields[key].isValid) {
+      returnForm.fields[key].input.style.backgroundColor = "white";
+    } else {
+      returnForm.fields[key].input.style.backgroundColor =
+        "rgba(255, 0, 0, 0.3)";
+    }
+  });
+
+  if (returnForm.allFieldsValid === true) {
+    setTimeout(() => {
+      alert("Thank you for filing in the form.");
+    }, 250);
   }
 }
 
 // This function checks and validates all the values of the different input fields
-function validate () {
-  let checkEmail = document.getElementById('exampleInputEmail1');
-  let checkName = document.getElementById('example-text-input');
-  let checkTextArea = document.getElementById('exampleTextarea');
+function validate() {
+  let emailInput = document.getElementById("exampleInputEmail1");
+  let nameInput = document.getElementById("example-text-input");
+  let textAreaInput = document.getElementById("exampleTextarea");
+  let inputs = {
+    fields: {
+      email: {
+        isValid: true,
+        input: emailInput,
+      },
+      name: {
+        isValid: true,
+        input: nameInput,
+      },
+      textArea: {
+        isValid: true,
+        input: textAreaInput,
+      },
+    },
+    allFieldsValid: true,
+  };
 
-  console.log(checkEmail);
-  if (checkEmail > 1) {
-    return true;
+  if (emailInput.value.length < 1 || !emailInput.value.includes("@")) {
+    inputs.fields.email.isValid = false;
+    inputs.allFieldsValid = false;
   }
-  else {
-    return false;
+  if (nameInput.value.length < 1) {
+    inputs.fields.name.isValid = false;
+    inputs.allFieldsValid = false;
   }
+  if (textAreaInput.value.length < 1) {
+    inputs.fields.textArea.isValid = false;
+    inputs.allFieldsValid = false;
+  }
+
+  return inputs;
 }
 
-document.getElementById('btn btn-primary-id').addEventListener('click', submitForm);
-
-validate();
+document
+  .getElementById("btn btn-primary-id")
+  .addEventListener("click", submitForm);
