dcc3336210a392a9e9ca3b726bf080d8fd1358a6
Shawen Aldred Harker <harkershawen@gmail.com>
2021-04-27 22:11:11 +0200

    Project 2
    The todos global object array isn't accessible in my addNewToDo function.

--- projects/2-todo-list/script.js
+++ projects/2-todo-list/script.js
@@ -1,3 +1,27 @@
+// These are the same todos that currently display in the HTML
+// You will want to remove the ones in the current HTML after you have created them using JavaScript
+let todos = [
+  { task: "Wash the dishes", completed: false },
+  { task: "Do the shopping", completed: false },
+];
+
+// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
+function addNewTodo(event) {
+  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
+  event.preventDefault();
+  // Write your code here... and remember to reset the input field to be blank after creating a todo!
+  
+  let addToDo = document.getElementById("todoInput");
+  console.log(addToDo.value);
+  addToDo.value = " ";
+  if (addToDo.value.length > 1) {
+    todos.push({ task: `${addToDo.value}`, completed: false});
+  }
+  
+}
+
+document.getElementById("btn-primary").addEventListener('click', addNewTodo);
+
 function populateTodoList(todos) {
   let list = document.getElementById("todo-list");
   // Write your code to create todo list elements with completed and delete buttons here, all todos should display inside the "todo-list" element.
@@ -8,31 +32,7 @@ function populateTodoList(todos) {
     listLiTag.innerText = todos[i].task;
   }
 }
-
-// These are the same todos that currently display in the HTML
-// You will want to remove the ones in the current HTML after you have created them using JavaScript
-let todos = [
-  { task: "Wash the dishes", completed: false },
-  { task: "Do the shopping", completed: false },
-  { task: "Go to the gym", completed: false },
-];
-
 populateTodoList(todos);
-
-// This function will take the value of the input field and add it as a new todo to the bottom of the todo list. These new todos will need the completed and delete buttons adding like normal.
-function addNewTodo() {
-  // The code below prevents the page from refreshing when we click the 'Add Todo' button.
-  // event.preventDefault();
-  // Write your code here... and remember to reset the input field to be blank after creating a todo!
-  
-  let addToDo = document.getElementById("todoInput");
-  if (addToDo.value.length < 1) {
-    return todos.push({ task: addToDo.value.length, completed: false});
-  }
-}
-
-// let addButton = document.getElementsByClassName("btn-primary");
-// addButton.addEventListener("click", addNewTodo);
 
 // Advanced challenge: Write a fucntion that checks the todos in the todo list and deletes the completed ones (we can check which ones are completed by seeing if they have the line-through styling applied or not).
 function deleteAllCompletedTodos() {
